c025fc989e76de054e835dfa03041e3e25b61fe8
ashishb888 <ashish.bhosle008@gmail.com>
2020-09-08 10:48:24 +0530

    config-client-b: Added an endpoint

--- config-client-b/src/main/java/poc/springcloud/ConfigClientBApplication.java
+++ config-client-b/src/main/java/poc/springcloud/ConfigClientBApplication.java
@@ -2,7 +2,10 @@ package poc.springcloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 @SpringBootApplication
 public class ConfigClientBApplication {
 
@@ -10,4 +13,8 @@ public class ConfigClientBApplication {
 		SpringApplication.run(ConfigClientBApplication.class, args);
 	}
 
+	@GetMapping("/")
+	public String hello() {
+		return "Hello there!\n";
+	}
 }
