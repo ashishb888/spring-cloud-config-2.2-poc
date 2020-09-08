19786ae43cf18d3237ab9848070b71614afcc43e
ashishb888 <ashish.bhosle008@gmail.com>
2020-09-08 10:47:57 +0530

    config-client-a: Added an endpoint

--- config-client-a/src/main/java/poc/springcloud/ConfigClientAApplication.java
+++ config-client-a/src/main/java/poc/springcloud/ConfigClientAApplication.java
@@ -2,7 +2,10 @@ package poc.springcloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 @SpringBootApplication
 public class ConfigClientAApplication {
 
@@ -10,4 +13,8 @@ public class ConfigClientAApplication {
 		SpringApplication.run(ConfigClientAApplication.class, args);
 	}
 
+	@GetMapping("/")
+	public String hello() {
+		return "Hello there!\n";
+	}
 }
