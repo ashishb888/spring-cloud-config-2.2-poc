20396af6cbf505c68c8f2f26f0e3a2637cad4904
ashishb888 <ashish.bhosle008@gmail.com>
2020-09-08 10:45:21 +0530

    config-client-a: Initial commit

---
+++ config-client-a/src/main/java/poc/springcloud/ConfigClientAApplication.java
@@ -0,0 +1,13 @@
+package poc.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConfigClientAApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConfigClientAApplication.class, args);
+	}
+
+}
